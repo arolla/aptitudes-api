a55f73bb334a309741ebd22b67b8f64d954e2d0b
Antoine Alberti <aa@antoinealberti.com>
2018-11-19 11:47:12 +0100

    describe "get employee" tests using Nested

--- src/test/kotlin/arolla/skillz/SkillzControllerTest.kt
+++ src/test/kotlin/arolla/skillz/SkillzControllerTest.kt
@@ -3,6 +3,7 @@ package arolla.skillz
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -21,20 +22,23 @@ class SkillzControllerTest {
                 .build()
     }
 
-    @Test
-    internal fun `returns requested employee`() {
-        every { skillzService.employee("johnny") } returns Employee("johnny", listOf(Skill("singing")))
-        restController
-                .perform(get("/employees/johnny"))
-                .andExpect(status().isOk)
-                .andExpect(content().json("""{name: "johnny", skills: [{name="singing"}]}"""))
-    }
+    @Nested
+    inner class `get employee` {
+        @Test
+        internal fun `returns requested employee`() {
+            every { skillzService.employee("johnny") } returns Employee("johnny", listOf(Skill("singing")))
+            restController
+                    .perform(get("/employees/johnny"))
+                    .andExpect(status().isOk)
+                    .andExpect(content().json("""{name: "johnny", skills: [{name="singing"}]}"""))
+        }
 
-    @Test
-    fun `returns 404 when employee doesn't exist`() {
-        every { skillzService.employee("noone") } returns null
-        restController
-                .perform(get("/employees/noone"))
-                .andExpect(status().isNotFound)
+        @Test
+        fun `returns 404 when employee doesn't exist`() {
+            every { skillzService.employee("noone") } returns null
+            restController
+                    .perform(get("/employees/noone"))
+                    .andExpect(status().isNotFound)
+        }
     }
 }
